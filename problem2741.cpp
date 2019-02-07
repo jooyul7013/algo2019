dcaf2635fe4dfaaaf7a24d41e7dc0fde9b4e4a20
Joo10 <jooyul7013@naver.com>
2019-02-07 18:33:31 +0900

    2741번 문제 풀이

---
+++ problem2741.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	int N;
+	cin>>N;
+	for(int i=1; i<N+1; i++)
+	{
+		cout<<i<<'\n';
+	}
+	
+	return 0;
+}
